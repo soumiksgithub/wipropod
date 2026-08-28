f033e05e38ed7d7d89f2d8ee72943c732d9f5d31
Soumik Ghosh <soumik.ghosh@gmail.com>
2026-08-28 18:09:23 +0000

    off added

--- app/src/main/java/com/qasuite/earpod/MainActivity.kt
+++ app/src/main/java/com/qasuite/earpod/MainActivity.kt
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     enum class NoiseMode(val label: String) {
         ANC("ANC"),
-        TRANSPARENCY("Transparency")
+        TRANSPARENCY("Transparency"),
+        OFF("Off")
     }
 
     private var currentMode: NoiseMode = NoiseMode.ANC
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         }
         binding.switchTransparency.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) updateMode(NoiseMode.TRANSPARENCY)
+        }
+        binding.switchOff.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) updateMode(NoiseMode.OFF)
         }
 
         binding.btnFindDevice.setOnClickListener { dummy("Find My Device") }
@@ -48,6 +52,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.switchAnc.isChecked = mode == NoiseMode.ANC
         binding.switchTransparency.isChecked = mode == NoiseMode.TRANSPARENCY
+        binding.switchOff.isChecked = mode == NoiseMode.OFF
     }
 
     private fun dummy(feature: String) {
